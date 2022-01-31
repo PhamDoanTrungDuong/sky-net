cb37ad3030545a6b3a3780d8d3b505997f2c4bd8
Trung Duong <pdtduong@gmail.com>
2022-01-31 16:56:02 +0700

    commit Client side

--- Client/src/app/app.component.ts
+++ Client/src/app/app.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
-
+import { IProduct } from './models/product';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { IPagination } from './models/pagination';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
-  title = 'Client';
+export class AppComponent implements OnInit {
+  title = 'Skynet';
+  products: IProduct[] = [];
+  constructor(private _http: HttpClient) {}
+
+  ngOnInit(): void {
+     this._http.get<IPagination>('https://localhost:5001/api/products?pageSize=50').subscribe(
+      (res: IPagination) => { 
+        // console.log(res);
+        this.products = res.data
+      },
+      (error) => {
+        console.log(error);
+      }
+    );
+  }
 }
