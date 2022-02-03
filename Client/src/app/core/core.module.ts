df9d50322aa63f8f1892ca9c82c4de85b48efdb6
Trung Duong <pdtduong@gmail.com>
2022-02-03 15:16:36 +0700

    commit client handling error

--- Client/src/app/core/core.module.ts
+++ Client/src/app/core/core.module.ts
@@ -2,14 +2,29 @@ import { RouterModule } from '@angular/router';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
+import { TestErrorComponent } from './test-error/test-error.component';
+import { ServerErrorComponent } from './server-error/server-error.component';
+import { NotFoundComponent } from './not-found/not-found.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ToastrModule, ToastContainerModule } from 'ngx-toastr';
 
 @NgModule({
-  declarations: [NavBarComponent],
+  declarations: [
+    NavBarComponent,
+    TestErrorComponent,
+    ServerErrorComponent,
+    NotFoundComponent,
+  ],
   imports: [
     CommonModule,
     RouterModule,
+    BrowserAnimationsModule,
+    ToastrModule.forRoot({
+      positionClass: 'toast-bottom-right',
+      preventDuplicates: true,
+    }),
+    ToastContainerModule,
   ],
-  exports: [NavBarComponent]
+  exports: [NavBarComponent],
 })
-export class CoreModule { }
+export class CoreModule {}
